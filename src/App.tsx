f681191a80bb521eb9020e8311598c0cb6b9abe8
chardetm <chardetm@gmail.com>
2023-09-27 18:46:22 +0200

    Test getting the state asynchronously

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { CapytaleRichTextEditor } from "capytale-rich-text-editor";
+import {
+  CapytaleRichTextEditor,
+  CapytaleRichTextEditorContext,
+  useCapytaleRichTextEditorContext,
+} from "capytale-rich-text-editor";
 import "capytale-rich-text-editor/style.css";
 import { useState } from "react";
 
@@ -6,57 +10,74 @@ import { JSONTree } from "react-json-tree";
 
 import "./App.css";
 
-const theme = {
-  scheme: "google",
-  author: "seth wright (http://sethawright.com)",
-  base00: "#1d1f21",
-  base01: "#282a2e",
-  base02: "#373b41",
-  base03: "#969896",
-  base04: "#b4b7b4",
-  base05: "#c5c8c6",
-  base06: "#e0e0e0",
-  base07: "#ffffff",
-  base08: "#CC342B",
-  base09: "#F96A38",
-  base0A: "#FBA922",
-  base0B: "#198844",
-  base0C: "#3971ED",
-  base0D: "#3971ED",
-  base0E: "#A36AC7",
-  base0F: "#3971ED",
-};
+import theme from "./theme.json";
 
 function App() {
+  return (
+    <CapytaleRichTextEditorContext>
+      <AppContent />
+    </CapytaleRichTextEditorContext>
+  );
+}
+
+function AppContent() {
   const [jsonOutput, setJsonOutput] = useState({});
   const [htmlOutput, setHtmlOutput] = useState("");
+  const [liveJson, setliveJson] = useState(true);
+  const capytaleRichTextEditorContext = useCapytaleRichTextEditorContext();
+
+  const asyncLoad = async () => {
+    if (!capytaleRichTextEditorContext.getState) {
+      alert("Méthode non chargée");
+    } else {
+      const state = await capytaleRichTextEditorContext.getState();
+      console.log("JSON :", state.json);
+      console.log("HTML :", state.html);
+      setJsonOutput(JSON.parse(state.json));
+      setHtmlOutput(state.html);
+    }
+  };
+
+  const onChange = !liveJson
+    ? undefined
+    : (json: string, html: string) => {
+        setJsonOutput(JSON.parse(json));
+        setHtmlOutput(html);
+      };
 
   return (
     <>
       <div className="editor-out-container">
         <CapytaleRichTextEditor
-          onChange={(json: string, html: string) => {
-            setJsonOutput(JSON.parse(json));
-            setHtmlOutput(html);
-          }}
+          onChange={onChange}
           isEditable={true}
           htmlInitialContent="<h1>Test</h1><p>Voici un <a href='https://google.fr/'>lien vers Google</a>.</p>"
         />
       </div>
       <div id="output">
         <div id="json-tree">
-          <JSONTree data={jsonOutput} theme={theme} invertTheme={true} hideRoot />
+          <JSONTree
+            data={jsonOutput}
+            theme={theme}
+            invertTheme={true}
+            hideRoot
+          />
         </div>
         <textarea
           id="json-text"
           value={JSON.stringify(jsonOutput, null, 2)}
           readOnly
         />
-        <textarea
-          id="html-text"
-          value={htmlOutput}
-          readOnly
-        />
+        <textarea id="html-text" value={htmlOutput} readOnly />
+      </div>
+      <div id="actions">
+        <button onClick={asyncLoad}>
+          Récupérer l'état de manière asynchrone
+        </button>{" "}
+        <button onClick={() => setliveJson(!liveJson)}>
+          {liveJson ? "Désactiver" : "Activer"} la mise à jour en temps réel du
+          JSON
+        </button>
       </div>
     </>
   );
